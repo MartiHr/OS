5ca606a077a54b3b94ded6df42f653c2c8658493
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-06-18 16:00:54 +0300

    Create 2021-SE-03.sh

---
+++ Скриптове/Жълти/2021-SE-03.sh
@@ -0,0 +1,30 @@
+# от цветилин
+#!/bin/bash
+
+if [[ ${#} -ne 2 ]] ; then
+        echo "Expected 2 arguments"
+        exit 1
+fi
+
+if [[ ! -f ${1} ]] ; then
+        echo "File does not exist"
+        exit 1
+fi
+
+
+BINARY=${1}
+OUTPUT=${2}
+
+touch ${OUTPUT}
+echo "#include <stdint.h>" > ${OUTPUT}
+
+ARR=""
+
+for UINT16 in $(xxd $BINARY | cut -d ' ' -f2-9) ; do
+        UINT16=$(echo "${UINT16}" | sed -E 's/^(..)(..)$/\2\1/' )
+        ARR="${ARR}0x${UINT16},"
+done
+
+ARR=$(echo "${ARR}" | sed -E "s/,$//")
+
+echo "uint16_t arr[] = { ${ARR} };" > ${OUTPUT}
