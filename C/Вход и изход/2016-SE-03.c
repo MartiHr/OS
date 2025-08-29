367f371a0a18f21ca7d27b4cd29e9059670a5ca2
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-08-29 14:26:57 +0300

    Create 2016-SE-03.c

---
+++ C/Вход и изход/2016-SE-03.c
@@ -0,0 +1,122 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <err.h>
+#include <errno.h>
+
+#define CHUNK_SIZE 10000000  // ~40 MB (10^7 uint32_t)
+#define MAX_CHUNKS 100
+
+// Compare function for qsort
+int compare_uint32(const void *a, const void *b) {
+    uint32_t x = *(uint32_t*)a;
+    uint32_t y = *(uint32_t*)b;
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
+}
+
+typedef struct {
+    int fd;
+    uint32_t current;
+    int finished;
+} TempFile;
+
+void read_next(TempFile *tf) {
+    ssize_t r = read(tf->fd, &tf->current, sizeof(uint32_t));
+    if (r == -1) err(1, "read");
+    if (r == 0) tf->finished = 1;
+    else if (r != sizeof(uint32_t)) errx(1, "partial read");
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2)
+        errx(1, "Usage: %s <binary_file>", argv[0]);
+
+    const char *filename = argv[1];
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) err(1, "open input file");
+
+    char temp_names[MAX_CHUNKS][20];
+    int chunk_count = 0;
+
+    while (1) {
+        uint32_t *buffer = malloc(CHUNK_SIZE * sizeof(uint32_t));
+        if (!buffer) err(1, "malloc");
+
+        ssize_t total_read = 0;
+        while (total_read < CHUNK_SIZE * sizeof(uint32_t)) {
+            ssize_t r = read(fd, (char*)buffer + total_read,
+                             CHUNK_SIZE * sizeof(uint32_t) - total_read);
+            if (r == -1) err(1, "read");
+            if (r == 0) break;
+            total_read += r;
+        }
+        size_t read_count = total_read / sizeof(uint32_t);
+        if (read_count == 0) {
+            free(buffer);
+            break;
+        }
+
+        qsort(buffer, read_count, sizeof(uint32_t), compare_uint32);
+
+        // Write temp file
+        snprintf(temp_names[chunk_count], 20, "chunk_%d.tmp", chunk_count);
+        int temp_fd = open(temp_names[chunk_count], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+        if (temp_fd == -1) err(1, "open temp");
+
+        ssize_t total_written = 0;
+        while (total_written < read_count * sizeof(uint32_t)) {
+            ssize_t w = write(temp_fd, (char*)buffer + total_written,
+                              read_count * sizeof(uint32_t) - total_written);
+            if (w == -1) err(1, "write temp");
+            total_written += w;
+        }
+
+        close(temp_fd);
+        free(buffer);
+        chunk_count++;
+    }
+
+    close(fd);
+
+    // Мерджване на temp файловете
+    TempFile temp_files[MAX_CHUNKS];
+    for (int i = 0; i < chunk_count; i++) {
+        temp_files[i].fd = open(temp_names[i], O_RDONLY);
+        if (temp_files[i].fd == -1) err(1, "open temp for merge");
+        temp_files[i].finished = 0;
+        read_next(&temp_files[i]);
+    }
+
+    int out_fd = open(filename, O_WRONLY | O_TRUNC);
+    if (out_fd == -1) err(1, "open output");
+
+    while (1) {
+        int min_index = -1;
+        for (int i = 0; i < chunk_count; i++) {
+            if (!temp_files[i].finished) {
+                if (min_index == -1 || temp_files[i].current < temp_files[min_index].current)
+                    min_index = i;
+            }
+        }
+        if (min_index == -1) break;
+
+        ssize_t w = write(out_fd, &temp_files[min_index].current, sizeof(uint32_t));
+        if (w == -1) err(1, "write output");
+        if (w != sizeof(uint32_t)) errx(1, "partial write output");
+
+        read_next(&temp_files[min_index]);
+    }
+
+    for (int i = 0; i < chunk_count; i++) {
+        close(temp_files[i].fd);
+        unlink(temp_names[i]);
+    }
+    close(out_fd);
+
+    return 0;
+}
